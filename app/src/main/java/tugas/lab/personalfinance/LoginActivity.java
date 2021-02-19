eed0a759bbb0870ebf1d24fa7cdb7fe34a193aa8
AveroStone <muhammadirsyad.ti.2019302029@gmailc.om>
2021-02-19 11:07:56 +0700

    Averroes

---
+++ app/src/main/java/tugas/lab/personalfinance/LoginActivity.java
@@ -0,0 +1,14 @@
+package tugas.lab.personalfinance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
