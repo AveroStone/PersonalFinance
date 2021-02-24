0c714d91d9a40becdfe307bc9d1a4f364eede98f
AveroStone <muhammadirsyad.ti.2019302029@gmailc.om>
2021-02-24 15:47:27 +0700

    Averroes

--- app/src/main/java/tugas/lab/personalfinance/Register.java
+++ app/src/main/java/tugas/lab/personalfinance/Register.java
@@ -61,40 +61,28 @@ public class Register extends AppCompatActivity {
         switch (month) {
             case 1:
                 return "JAN";
-            break;
             case 2:
                 return "FEB";
-            break;
             case 3:
                 return "MAR";
-            break;
             case 4:
                 return "APR";
-            break;
             case 5:
                 return "MAY";
-            break;
             case 6:
                 return "JUN";
-            break;
             case 7:
                 return "JUL";
-            break;
             case 8:
                 return "AUG";
-            break;
             case 9:
                 return "SEP";
-            break;
             case 10:
                 return "OCT";
-            break;
             case 11:
                 return "NOV";
-            break;
             case 12:
                 return "DEC";
-            break;
             default:
                 return "JAN";
         }
